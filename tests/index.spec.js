ea3361db451aa9e044afa7b8cf64970805be7934
Rogues <rogues@es6.today>
2017-08-29 23:54:58 -0500

    [feat] Enable JavaScript Unit Testing.

---
+++ tests/index.spec.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('FlappyBird Test Suite', () => {
+  it('is written by Yair, a.k.a. @agzeri', () => {
+    const GAME = 'Flappy Bird';
+
+    expect(GAME).to.equal('Flappy Bird');
+  });
+});
