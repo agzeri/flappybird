0e0b677b105e414ac3a1c25129795f353b37ba85
Rogues <rogues@es6.today>
2017-09-04 12:22:12 -0500

    [tool] Enable Code Coverage with nyc.

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,9 +1,16 @@
 import { expect } from 'chai';
+import name from '../app/author';
 
 describe('FlappyBird Test Suite', () => {
   it('is written by Yair, a.k.a. @agzeri', () => {
     const GAME = 'Flappy Bird';
 
     expect(GAME).to.equal('Flappy Bird');
+  });
+
+  it('return the author name', () => {
+    const actual = name('agzeri');
+
+    expect(actual).to.equal('@agzeri');
   });
 });
